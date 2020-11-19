cc65d3bb2917095d883cb82b9582279797d49d36
yipeyayeah <60258061+yipeyayeah@users.noreply.github.com>
2020-11-19 11:49:34 +0800

    First commit blah blah

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSC2001_LAB11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
